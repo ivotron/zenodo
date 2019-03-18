00898f531f1fc05e3b2bed6dca4776a775253d00
arshul <arshulmohd123@gmail.com>
2019-03-19 01:16:35 +0530

    updated paths and docker file to install required package

--- publish_deposition/entrypoint.sh
+++ publish_deposition/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-if [[ -e ../create_deposition/resp.json ]] && [[ -z $ZENODO_DEPOSIT_ID ]];
+if [[ -e ./create_deposition/resp.json ]] && [[ -z $ZENODO_DEPOSIT_ID ]];
 then
-    export ZENODO_DEPOSIT_ID=$(jq '.id' ../create_deposition/resp.json)
+    export ZENODO_DEPOSIT_ID=$(jq '.id' ./create_deposition/resp.json)
 fi
-curl --request POST https://sandbox.zenodo.org/api/deposit/depositions/$ZENODO_DEPOSIT_ID/actions/publish?access_token=$ZENODO_API_TOKEN -o resp.json \
- && \
- if [[ -e ../create_deposition/resp.json ]]; then
-    rm ../create_deposition/resp.json
- fi
+curl --request POST https://sandbox.zenodo.org/api/deposit/depositions/$ZENODO_DEPOSIT_ID/actions/publish?access_token=$ZENODO_API_TOKEN
