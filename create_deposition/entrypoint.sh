a61ca576939fb251e6d13d71adb764e27d8b680e
arshul <arshulmohd123@gmail.com>
2019-03-19 01:16:35 +0530

    updated paths and docker file to install required package

--- create_deposition/entrypoint.sh
+++ create_deposition/entrypoint.sh
@@ -4,4 +4,4 @@ curl --header "Content-Type: application/json" \
       --request POST \
       --data @$ZENODO_METADATA \
       https://sandbox.zenodo.org/api/deposit/depositions?access_token=$ZENODO_API_TOKEN \
-      -o resp.json
+      -o ./create_deposition/resp.json
