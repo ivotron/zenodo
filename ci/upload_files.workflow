fc82bd172b7d64ebf631cff9372c0054cd9965c3
arshul <arshulmohd123@gmail.com>
2019-03-19 01:14:43 +0530

    updated paths and docker file to install required package

--- ci/upload_files.workflow
+++ ci/upload_files.workflow
@@ -2,10 +2,10 @@ workflow "Upload Files" {
   resolves = "upload files"
 }
 action "upload files" {
-  uses = "upload_files/"
+  uses = "./upload_files"
   secrets = ["ZENODO_API_TOKEN"]
   env = {
     ZENODO_DEPOSIT_ID = ""
-    UPLOAD_FILES_DIRECTORY = "../files"
+    UPLOAD_FILES_DIRECTORY = "./files"
   }
 }
--- upload_files/entrypoint.sh
+++ upload_files/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
-if [[ -e "../create_deposition/resp.json" ]] && [[ -z "$ZENODO_DEPOSIT_ID" ]];
+if [[ -e "./create_deposition/resp.json" ]] && [[ -z "$ZENODO_DEPOSIT_ID" ]];
 then
-    export ZENODO_DEPOSIT_ID=$(jq '.id' ../create_deposition/resp.json)
+    export ZENODO_DEPOSIT_ID=$(jq '.id' ./create_deposition/resp.json)
 fi
 
 if [[ -d "$UPLOAD_FILES_DIRECTORY" ]] && [ "$(ls -A $UPLOAD_FILES_DIRECTORY)" ];
@@ -11,7 +11,7 @@ then
   for file in "$UPLOAD_FILES_DIRECTORY"/*; do
     echo "$file"
     curl -F file=@"$file" \
-    https://sandbox.zenodo.org/api/deposit/depositions/$ZENODO_DEPOSIT_ID/files?access_token=$ZENODO_API_TOKEN -o resp.json
+    https://sandbox.zenodo.org/api/deposit/depositions/$ZENODO_DEPOSIT_ID/files?access_token=$ZENODO_API_TOKEN
   done
 else
   echo "No files directory or empty directory"
