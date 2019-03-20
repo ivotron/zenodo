4932e22fed1802a45fde574954718602bbf9c5c1
arshul <arshulmohd123@gmail.com>
2019-03-20 18:37:10 +0530

    Updated docker image,added path to repo in uses, updated output path, updated readme

--- create_deposition/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-curl --header "Content-Type: application/json" \
-      --request POST \
-      --data @$ZENODO_METADATA \
-      https://sandbox.zenodo.org/api/deposit/depositions?access_token=$ZENODO_API_TOKEN \
-      -o ./create_deposition/resp.json
