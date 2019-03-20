cbe67c6fa1385aef8954c04366b1d7c5ca52a7f4
arshul <arshulmohd123@gmail.com>
2019-03-20 18:45:51 +0530

    tested and updated repo reference

--- create/entrypoint.sh
+++ create/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 set -ex
+if [[ -z "$ZENODO_USE_SANDBOX" ]]; then
+    BASE_URL="https://zenodo.org/api/deposit/depositions"
+else
+    BASE_URL="https://sandbox.zenodo.org/api/deposit/depositions"
+fi
 curl --header "Content-Type: application/json" \
       --request POST \
       --data @$ZENODO_METADATA_PATH \
-      https://sandbox.zenodo.org/api/deposit/depositions?access_token=$ZENODO_API_TOKEN \
+      $BASE_URL?access_token=$ZENODO_API_TOKEN \
       -o $GITHUB_WORKSPACE/create_resp.json
